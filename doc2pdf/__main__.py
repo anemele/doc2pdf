aeba2f3fad03f36e898defc78179fabdc697386e
Jason Swift <anemele@outlook.com>
2024-01-02 00:04:34 +0800

    update

--- doc2pdf/__main__.py
+++ doc2pdf/__main__.py
@@ -3,20 +3,17 @@
 import argparse
 import glob
 import os.path
-import sys
 from itertools import chain
 from pathlib import Path
 
 from .core import convert
 
-parser = argparse.ArgumentParser(
-    prog=__package__ if len(sys.argv) == 1 else sys.argv[1], description=__doc__
-)
+parser = argparse.ArgumentParser(prog=__package__, description=__doc__)
 parser.add_argument(
     'file', nargs='+', type=str, help='Word file (.doc, .docx), glob supports'
 )
 
-args = parser.parse_args(sys.argv[2:])
+args = parser.parse_args()
 args_file: list[str] = args.file
 
 paths = map(
